d10dd087389d63bb2755800fe62e076cc5297163
Gennady Kovshenin <gennady@kovshenin.com>
2018-02-27 00:26:24 +0500

    Better URL inhertion, tests

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,16 @@
 extern crate reqwest;
 extern crate serde_json;
+extern crate linkify;
 
 use serde_json::Value;
 use std::time::Duration;
+use linkify::LinkFinder;
 
 fn main() {
 	let botkey = "botKEYHERE";
 	let apiurl = "https://api.telegram.org";
 
-	let mut offset = String::from("575719478");
+	let mut offset = String::from("575719649");
 
 	let client = reqwest::Client::builder()
 			.timeout(Duration::from_secs(90))
@@ -35,11 +37,7 @@ fn main() {
 			if result["message"]["text"].is_string() {
 				let text = result["message"]["text"].as_str().unwrap();
 
-				if text.to_lowercase().contains("wordpress")
-					&& ! text.to_lowercase().contains("wordpress.com")
-					&& ! text.to_lowercase().contains("wordpress.org")
-					&& ! text.contains("WordPress") {
-						
+				if capital_p_dangit(text) {
 						println!("Received: {:?}", result["message"]["text"]);
 						println!("{:?}", result["message"]["chat"]["id"].as_i64());
 
@@ -59,3 +57,37 @@ fn main() {
 		}
 	}
 }
+
+fn capital_p_dangit(text: &str) -> bool {
+	// Strip any links we have and call again
+	let finder = LinkFinder::new();
+
+	let links: Vec<_> = finder.links(text).collect();
+	if links.len() > 0 {
+		let mut _text = String::from(text);
+		for link in links {
+			_text = _text.replace(link.as_str(), "");
+		}
+		return capital_p_dangit(_text.as_str());
+	}
+
+	if text.to_lowercase().contains("wordpress.org") || text.to_lowercase().contains("wordpress.com") {
+		return capital_p_dangit(text.to_lowercase().replace("wordpress.org", "").replace("wordpress.com", "").as_str());
+	}
+
+	text.to_lowercase().contains("wordpress") && ! text.contains("WordPress")
+}
+
+#[test]
+fn test_capital_p_dangit() {
+	assert!(true == capital_p_dangit("wordpress"));
+	assert!(true == capital_p_dangit("Wordpress"));
+	assert!(true == capital_p_dangit("WORDPRESS"));
+	assert!(false == capital_p_dangit("WordPress"));
+	assert!(false == capital_p_dangit("wordpress WordPress"));
+	assert!(false == capital_p_dangit("wordpress.org"));
+	assert!(false == capital_p_dangit("wordpress.com"));
+	assert!(false == capital_p_dangit("check out https://someothersite.com/wordpress/hello"));
+	assert!(true == capital_p_dangit("check out https://wordpress.org Wordpress"));
+}
+						
